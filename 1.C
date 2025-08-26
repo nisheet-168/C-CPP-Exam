e6ffdeb7f295a4bee5bdffe8c609f50d6c81cd44
Nisheet Trivedi <nisheettrivedi@gmail.com>
2025-08-26 17:56:20 +0530

    Create 1.C

---
+++ 1.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    if (num1 < num2) {
+        printf("Minimum is: %d\n", num1);
+    } else {
+        printf("Minimum is: %d\n", num2);
+    }
+
+    return 0;
+}
